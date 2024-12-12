4083b69458365eddbd77a33ca58f7ac26b0cdd56
Abdul Jabbar <abduljabbarkhilji.xwave@gmail.com>
2024-12-12 13:24:24 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <>
+<h1>Abdul Jabbar</h1>
+    </>
+  );
+}
+
+export default App;
